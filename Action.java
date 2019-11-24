6700a6442774545e8c35eedc17351987d01acea5
albertc6 <55066992+albertc6@users.noreply.github.com>
2019-11-23 23:31:39 -0600

    Finished Action commands, started Room imp.

---
+++ Action.java
@@ -0,0 +1,26 @@
+public class Action {
+	
+	private String action;
+	private String object;
+	
+	public Action(String action, String object) {
+		this.action = action;
+		this.object = object;
+	}
+	
+	public String getAction() {
+		return action;
+	}
+	
+	public String getObject() {
+		return object;
+	}
+	
+	public boolean hasAction() {
+		return (action == null);
+	}
+	
+	public boolean hasObject() {
+		return (object != null);
+	}
+}
---
+++ ActionWords.java
@@ -0,0 +1,28 @@
+public class ActionWords {
+	
+	private static final String actions[] = {
+			"go", "quit", "help"
+	};
+	
+	public ActionWords() {
+		
+	}
+	
+	public boolean checkAction(String action) {
+		
+		for(int i = 0; i < actions.length; i++) {
+			if (actions[i].equals(action)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void showActions() {
+		
+		for (int i = 0; i < actions.length; i++) {
+			System.out.println(actions[i] + "   ");
+		}
+		System.out.println();
+	}
+}
